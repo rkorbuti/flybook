1c6b8fa9fecf0493a61d00bce5ca09323213fb33
stas.mavrov <titos@bigmir.net>
2014-09-06 10:25:49 +0300

    Changed airports_http_request code to save JSON response in a file as: 'airport_country - airport_name - airport_code'

--- Temp_Code/airports_http_request.groovy
+++ Temp_Code/airports_http_request.groovy
@@ -6,6 +6,12 @@ import static groovyx.net.http.ContentType.JSON
 
 def http = new HTTPBuilder('http://www.ryanair.com')
 
+def listOfAirports = [ : ]
+
 http.get(path: '/en/api/2/forms/flight-booking-selector/') {resp, json ->
-    println json.airports.name
-}
+    json.airports.each { listOfAirports.put(it.iataCode, [it.country.name, it.name])}}
+    
+
+new File('E:\\httpreq.txt').withWriter {out ->
+    listOfAirports.each() {key, value ->
+        out.write("${value[0]} - ${value[1]} - ${key}" + "\n")}}
