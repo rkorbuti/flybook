a3dae7e2d6443673f0a676ca0b729a257af68bcd
roman.korbutiak <roman.korbutiak@avid.com>
2014-09-06 22:51:13 +0300

    Fixed data in drop down were shown id's fro City and Country. Now it fixed and showing corresponding names.

--- grails-app/domain/flybook/Country.groovy
+++ grails-app/domain/flybook/Country.groovy
@@ -3,6 +3,9 @@ package flybook
 class Country {
 
     String name
+    String toString(){
+        "${name}"
+    }
 
     static constraints = {
         name(nullable: false, blank: false, unique: true)
