389e67a31be078bdb75a8c21b1c42f5671aece19
Evgeniy Malykhin <55jeka@gmail.com>
2014-09-14 11:40:10 +0300

    Add: REST client plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,6 +67,9 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
+        // used for httpBuilder
+        compile ":rest:0.8"
+
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
         //compile ":less-asset-pipeline:1.10.0"
